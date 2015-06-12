6413d777a59f80eb1b8359c4f0fa0a97333ef012
김동범 <dongbum9@gmail.com>
2015-06-12 17:03:52 +0900

    refs #173 : MySQL 연결 클래스 추가 * 기초적인 코드 작성 * main.cpp에 테스트 코드 추가

--- src/dserver/database/mysql/mysql_conn.h
+++ src/dserver/database/mysql/mysql_conn.h
@@ -8,6 +8,7 @@
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
 
 namespace dserver
 {
@@ -23,7 +24,19 @@ public:
 	virtual ~MysqlConn(void);
 
 	bool	Connect(void);
+	void	Disconnect(void);
+
+	void	PrepareQuery(char* query_info);
+	void	Execute(void);
+	bool	Fetch(void);
+
+	int32_t		GetInt(void);
 private:
+	sql::Driver*				driver_;
+	sql::Connection*			conn_;
+	sql::PreparedStatement*		pstmt_;
+	sql::ResultSet*				resultset_;
+
 };
 
 }
