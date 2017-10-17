c5eccfea195a0eb0f0082352c1f1cba18211523b
김동범 <dongbum9@gmail.com>
2017-10-17 20:35:07 +0900

    refs #224 : 관리용 웹서버 추가 * 작업 내용 반영

--- src/dserver/http/http_service.cpp
+++ src/dserver/http/http_service.cpp
@@ -11,5 +11,66 @@ HTTPService::~HTTPService(void)
 
 void HTTPService::StartHandler(void)
 {
+	boost::asio::async_read_until(
+		*socket_ptr_,
+		request_,
+		'\n',
+		[this](
+			const ErrorCode& ec,
+			size_t bytes_transferred)
+	{
+		RequestHandler(
+			ec,
+			bytes_transferred
+		);
+	}
+	);
+}
 
+void HTTPService::RequestHandler(const ErrorCode & ec, size_t bytes_transferred)
+{
+	if (0 != ec)
+	{
+		std::cout << "Error" << ec.value() << ec.message() << std::endl;
+
+		Finish();
+		return;
+	}
+
+	response_ = ProcessRequest(request_);
+
+	boost::asio::async_write(
+		*socket_ptr_,
+		boost::asio::buffer(response_),
+		[this](
+			const ErrorCode& ec,
+			size_t bytes_transferred
+			)
+	{
+
+	}
+	);
+}
+
+std::string HTTPService::ProcessRequest(StreamBuf& request)
+{
+	std::string response = "Response\n";
+	return response;
+}
+
+void HTTPService::ResponseHandler(const ErrorCode& ec, size_t bytes_transferred)
+{
+	if (0 != ec)
+	{
+
+	}
+
+	Finish();
+}
+
+
+
+void HTTPService::Finish(void)
+{
+	delete this;
 }
--- src/dserver/http/http_service.h
+++ src/dserver/http/http_service.h
@@ -5,12 +5,19 @@
 class HTTPService
 {
 public:
+	typedef boost::asio::streambuf	StreamBuf;
+
 	HTTPService(std::shared_ptr<Socket> socket_ptr);
 	virtual ~HTTPService(void);
 
 	void StartHandler(void);
+	void RequestHandler(const ErrorCode& ec, size_t bytes_transferred);
+	std::string ProcessRequest(StreamBuf& request);
+	void ResponseHandler(const ErrorCode& ec, size_t bytes_transferred);
+	void Finish(void);
 
 private:
-	std::shared_ptr<Socket> socket_ptr_;
-
+	std::shared_ptr<Socket>		socket_ptr_;
+	std::string					response_;
+	StreamBuf					request_;
 };
